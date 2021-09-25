3620d52aa28e8b5537f2aa48c4c675d649610b10
Kevin Sankar <dev@kmsankar.com>
2021-09-24 20:44:21 -0400

    Measured performance of a single image

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -21,6 +21,8 @@ public class CameraScript : MonoBehaviour
 
     float intervalTime;
     float timeElapsed;
+    float[] renderTimes = new float[3];
+    float[] times = new float[4];
     int imageCountIdeal = 0;
     int imageCountActual = 0;
 
@@ -38,11 +40,18 @@ public class CameraScript : MonoBehaviour
             imageWidth = cameraObject.targetTexture.width;
             imageHeight = cameraObject.targetTexture.height;
         }
+
+        cameraObject.gameObject.SetActive(false);
     }
 
     void Start() 
     {
         intervalTime = Time.fixedTime + (1.0f/cameraFrequency);
+
+        // Add your callback to the delegate's invocation list
+        Camera.onPreCull += OnPreCullCallback;
+        Camera.onPreRender += OnPreRenderCallback;
+        Camera.onPostRender += OnPostRenderCallback;
     }
 
     void Update() 
@@ -65,16 +74,75 @@ public class CameraScript : MonoBehaviour
             intervalTime = Time.fixedTime + (1.0f/cameraFrequency);
         }
     }
+    
+    void OnPreCullCallback(Camera cam) 
+    {
+        if (cam == cameraObject)
+        {
+            renderTimes[0] = Time.realtimeSinceStartup;
+        }
+    }
+
+    void OnPreRenderCallback(Camera cam) 
+    {
+        if (cam == cameraObject)
+        {
+            renderTimes[1] = Time.realtimeSinceStartup;
+        }
+    }
+
+    void OnPostRenderCallback(Camera cam) 
+    {
+        if (cam == cameraObject)
+        {
+            renderTimes[2] = Time.realtimeSinceStartup;
+        }
+    }
 
     void CallTakeImage()
     {
+        // Create time variables
+        float startTime = 1.0f;
+        float endTime = 1.0f;
+
+        // Create a texture in RGB24 format with the specified width and height
         Texture2D image = new Texture2D(imageWidth, imageHeight, TextureFormat.RGB24, false);
+
+        // RENDER
+        // Render the camera's view
+        // The camera will send OnPreCull, OnPreRender and OnPostRender
+        // OnPreCull - Event function that Unity calls before a Camera culls the scene
+        // OnPreRender - Event function that Unity calls before a Camera renders the scene
+        // OnPostRender - Event function that Unity calls after a Camera renders the scene
+        startTime = Time.realtimeSinceStartup;
         cameraObject.Render();
+        endTime = Time.realtimeSinceStartup;
+        times[0] = (endTime - startTime) * 1000;
+
+        // The Render Texture in RenderTexture.active is the one that will be read by ReadPixels
         RenderTexture.active = cameraObject.targetTexture;
+
+        // READ/COPY
+        // Read the active render texture into the image texture (from screen to image texture)
+        startTime = Time.realtimeSinceStartup;
         image.ReadPixels(new Rect(0, 0, imageWidth, imageHeight), 0, 0);
+        endTime = Time.realtimeSinceStartup;
+        times[1] = (endTime - startTime) * 1000;
+
+        // ENCODE/COMPRESS
+        // Encode the texture in JPG format
+        startTime = Time.realtimeSinceStartup;
         byte[] bytes = image.EncodeToJPG(cameraQuality);
+        endTime = Time.realtimeSinceStartup;
+        times[2] = (endTime - startTime) * 1000;
+
+        // WRITE/SAVE
+        // Write the returned byte array to a file
         string filename = ImageName();
+        startTime = Time.realtimeSinceStartup;
         System.IO.File.WriteAllBytes(filename, bytes);
+        endTime = Time.realtimeSinceStartup;
+        times[3] = (endTime - startTime) * 1000;
     }
 
     string ImageName()
@@ -93,8 +161,21 @@ public class CameraScript : MonoBehaviour
             imageCountIdeal = (int)(timeElapsed * cameraFrequency);
         }
         
+        float cullTime = renderTimes[1] - renderTimes[0];
+        float renderTime = renderTimes[2] - renderTimes[1];
+
         Debug.Log("Time Elasped: " + timeElapsed);
-        Debug.Log("Image Count (ideal):" + imageCountIdeal);
-        Debug.Log("Image Count (actual):" + imageCountActual);
+        Debug.Log("Image Count: " + imageCountIdeal + " (ideal), " + imageCountActual + " (actual)");
+        Debug.Log("PreCull: " + renderTimes[0] + ", PreRender: " + renderTimes[1] + ", PostRender: " + renderTimes[2]);
+        Debug.Log("Cull Time: " + cullTime + ", Render Time: " + renderTime);
+        Debug.Log("Render: " + times[0] + " ms, Read/Copy: " + times[1] + " ms, Encode/Compress: " + times[2] + " ms, Write/Save: " + times[3] + " ms");
+    }
+
+    void OnDestroy()
+    {
+        // Remove your callback from the delegate's invocation list
+        Camera.onPreCull -= OnPreCullCallback;
+        Camera.onPreRender -= OnPreRenderCallback;
+        Camera.onPostRender -= OnPostRenderCallback;
     }
 }
